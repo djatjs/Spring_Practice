aeae3a2ef7498eb902e6cbc75f2eeee5dd9e408a
Sunil <umsunil2@naver.com>
2025-03-24 11:11:48 +0900

    .

--- src/main/java/kr/kh/tmp/service/MemberServiceImp.java
+++ src/main/java/kr/kh/tmp/service/MemberServiceImp.java
@@ -2,6 +2,8 @@ package kr.kh.tmp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.kh.tmp.dao.MemberDAO;
 
@@ -9,4 +11,10 @@ import kr.kh.tmp.dao.MemberDAO;
 public class MemberServiceImp implements MemberService {
 	@Autowired
 	MemberDAO memberDao;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		
+		return "home";
+	}
 }
